8f0380dc948961904df6795ea2c8910babbefc3b
ChitteGnanaPrasannKumarReddy <116110463+ChitteGnanaPrasannKumarReddy@users.noreply.github.com>
2025-01-05 17:00:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 // This is the boilerplate code given for you
 // You can modify this code
 // Product data
-// Product data
 const products = [
-  { id: 1, name: "Product 1", price: 20 },
-  { id: 2, name: "Product 2", price: 40 },
-  { id: 3, name: "Product 3", price: 60 },
-  { id: 4, name: "Product 4", price: 70 },
-  { id: 5, name: "Product 5", price: 90 },
+  { id: 1, name: "Product 1", price: 10 },
+  { id: 2, name: "Product 2", price: 20 },
+  { id: 3, name: "Product 3", price: 30 },
+  { id: 4, name: "Product 4", price: 40 },
+  { id: 5, name: "Product 5", price: 50 },
 ];
 
 // DOM elements
