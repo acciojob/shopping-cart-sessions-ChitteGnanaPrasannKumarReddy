73c5d530446358dff2c7789bf9905ab4b9d7a411
ChitteGnanaPrasannKumarReddy <116110463+ChitteGnanaPrasannKumarReddy@users.noreply.github.com>
2025-01-05 17:18:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
 // You can modify this code
 // Product data
 const products = [
-  {"id":1,"name":"Product 1","price":10},{"id":5,"name":"Product 5","price":50},{"id":1,"name":"Product 1","price":10},
-  { id: 2, name: "product 2", price: 20 },
-  { id: 3, name: "product 3", price: 30 },
-  { id: 4, name: "product 4", price: 40 },
-  { id: 5, name: "product 5", price: 50 },
+  {"id":1,"name":"Product 1","price":10},
+  {"id":5,"name":"Product 5","price":50},
+  {"id":1,"name":"Product 1","price":10},
+  { id: 2, name: "Product 2", price: 20 },
+  { id: 3, name: "Product 3", price: 30 },
+  { id: 4, name: "Product 4", price: 40 },
+  { id: 5, name: "Product 5", price: 50 },
 ];
 
 // DOM elements
